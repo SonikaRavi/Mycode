fcc098f47ef4cb12a8a6ec72e708eb9a886a199c
SonikaRavi <sonika349@gmail.com>
2016-06-09 15:24:10 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n &1)
+	{
+		cout<<"odd";
+	}
+	else
+	{
+		cout<<"even";
+	}
+	return 0;
+}
