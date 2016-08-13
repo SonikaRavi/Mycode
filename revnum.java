de6fbfb75ea9f7f0bbde32846f1c76c28d8aa29c
SonikaRavi <sonika349@gmail.com>
2016-08-13 21:37:09 +0530

    Create revnum.java

---
+++ revnum.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner in=new Scanner(System.in);
+        String str=in.nextLine();
+        StringBuffer sb=new StringBuffer(str);
+        sb.reverse();
+        String s=sb.toString();
+        int i=Integer.valueOf(s);
+        
+
+        System.out.println(i);
+    }
+}
